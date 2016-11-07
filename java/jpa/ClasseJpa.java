1e66e408fda6782322b4fbae671885042a2bdc36
laye <mamallaye@gmail.com>
2016-11-07 19:44:34 +0000

    modfication des couches jpa

--- java/jpa/ClasseJpa.java
+++ java/jpa/ClasseJpa.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *c'est la classe d'accés au données 
  */
 package jpa;
 
